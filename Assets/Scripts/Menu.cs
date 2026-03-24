702cffa1b5d153af911f41b767785950ce4f674f
thakitttt <thakit.pech@bumail.net>
2026-03-24 14:29:04 +0700

    Menu update

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,10 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-
     public void playGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void menuGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void reTry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Next()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(3);
     }
 
     public void QuitGame()
